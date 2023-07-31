e09addeef94044bab34fc4d205a36dad91de5664
Daniel Pakoca <danielpakoca@gmail.com>
2023-07-31 12:44:49 +0200

    Name changed

--- components/BaseLayout/Navigation/NavigationSidebar.js
+++ components/BaseLayout/Navigation/NavigationSidebar.js
@@ -11,7 +11,7 @@ export const NavigationBar = ({setNavOpen, handleLogout}) => {
     <nav className="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
       <div className="flex items-center mb-8">
         <Link className="mr-auto text-3xl font-bold leading-none" href="/">
-          TinderMatch
+          Tinder
         </Link>
         <button onClick={() => setNavOpen(false)} className="navbar-close">
           <svg
